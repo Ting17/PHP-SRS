6f4c2c6e9814c303988e98f0c4cea48ecd5ea57e
coven1688 <coven1688@gmail.com>
2016-10-25 22:03:16 +0800

    Fixed based on feedback from Sprint 1

--- PHP-SRS/api/insertProduct.php
+++ PHP-SRS/api/insertProduct.php
@@ -7,8 +7,41 @@
         $prod_price = $_POST['prod_price']; 
         $Manufacture = $_POST['Manufacture'];
         $Category = $_POST['Category'];
-        $Manu_date = $_POST['Manu_date'];
+
+        $Month_Array = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+        $Month_Number = array("01","02","03","04","05","06","07","08","09","10","11","12");
+
         $Expiry_date = $_POST['Expiry_date'];
+        $Expiry_date = explode(" ",$Expiry_date);
+
+        for($i=0;$i<12;$i++)
+        {
+            if($Expiry_date[1] == $Month_Array[$i])
+            {
+                $Expiry_date[1] = $Month_Number[$i];
+            }
+        }
+
+        $Expiry_date = $Expiry_date[3]."-".$Expiry_date[1]."-".$Expiry_date[2];
+
+        /***********************************************************************/
+        
+        $Manu_date = $_POST['Manu_date'];
+        $Manu_date = explode(" ",$Manu_date);
+
+        for($i=0;$i<12;$i++)
+        {
+            if($Manu_date[1] == $Month_Array[$i])
+            {
+                $Manu_date[1] = $Month_Number[$i];
+            }
+        }
+
+        $Manu_date = $Manu_date[3]."-".$Manu_date[1]."-".$Manu_date[2];
+
+
+        //$Manu_date = $_POST['Manu_date'];
+        //$Expiry_date = $_POST['Expiry_date'];
         
         $sql = "INSERT INTO product(prod_name, prod_desc, prod_price, Manufacture, Category, Manu_date, Expiry_date) VALUES('". $prod_name ."','". $prod_desc ."','". $prod_price ."','". $Manufacture ."','". $Category ."','". $Manu_date ."','". $Expiry_date ."');";
         
--- PHP-SRS/api/updateProduct.php
+++ PHP-SRS/api/updateProduct.php
@@ -39,13 +39,44 @@
 
         if($put_vars['Manu_date'] !== '')
         {
+            $Month_Array = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+            $Month_Number = array("01","02","03","04","05","06","07","08","09","10","11","12");
+
             $Manu_date = $put_vars['Manu_date'];
+            $Manu_date = explode(" ",$Manu_date);
+
+            for($i=0;$i<12;$i++)
+            {
+                if($Manu_date[1] == $Month_Array[$i])
+                {
+                    $Manu_date[1] = $Month_Number[$i];
+                }
+            }
+
+            $Manu_date = $Manu_date[3]."-".$Manu_date[1]."-".$Manu_date[2];
+
             $sql = "UPDATE product SET Manu_date = '" . $Manu_date . "' WHERE prod_id = '". $prod_id ."';";
         }
 
         if($put_vars['Expiry_date'] !== '')
         {
+
+            $Month_Array = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+            $Month_Number = array("01","02","03","04","05","06","07","08","09","10","11","12");
+
             $Expiry_date = $put_vars['Expiry_date'];
+            $Expiry_date = explode(" ",$Expiry_date);
+
+            for($i=0;$i<12;$i++)
+            {
+                if($Expiry_date[1] == $Month_Array[$i])
+                {
+                    $Expiry_date[1] = $Month_Number[$i];
+                }
+            }
+
+            $Expiry_date = $Expiry_date[3]."-".$Expiry_date[1]."-".$Expiry_date[2];
+
             $sql = "UPDATE product SET Expiry_date = '" . $Expiry_date . "' WHERE prod_id = '". $prod_id ."';";
         }
         
