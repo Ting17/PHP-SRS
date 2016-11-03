c4e29aded063711e02b4e9ceb5b0f000f5d84787
coven1688 <coven1688@gmail.com>
2016-11-03 22:09:59 +0800

    Unit testing for Sales Report

--- PHP-SRS/api/getSalesR.php
+++ PHP-SRS/api/getSalesR.php
@@ -1,7 +1,25 @@
 <?php
     include('dbconnect.php');
+    $num = 0;
     $result = $conn->query("SELECT * FROM tbl_sales;");
 	$outp = "";
+
+/* Testing Part */
+    $testing = $conn->query("SELECT * FROM tbl_sales;");
+
+    while($rs = $testing->fetch_array(MYSQLI_ASSOC)) {
+        $retrieve_testing = "SELECT prod_name FROM product WHERE prod_id ='".$rs["prod_id"]."'";
+        $testing2 = $conn->query($retrieve_testing);
+        if ($testing2->num_rows > 0) 
+        {
+            // output data of each row
+            while($row = $testing2->fetch_assoc()) 
+            {
+                $num = $num + 1;
+            }
+        }
+    }
+/* /Testing Part */
 
 	while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
 
@@ -14,7 +32,6 @@
             while($row = $result2->fetch_assoc()) 
             {
                 $prod_name = $row["prod_name"];
-                //$outp .= '"prod_name":"'  . $prod_name . '"}';
             }
         }
 
@@ -34,7 +51,8 @@
 		$outp .= '"sales_quantity":"'  . $rs["sales_quantity"] . '",';
 		$outp .= '"sales_date":"'  . $rs["sales_date"] . '",';
 		$outp .= '"member_id":"'  . $rs["member_id"] . '",';
-        $outp .= '"sales_price":"'  . $rs["sales_price"] . '"}';
+        $outp .= '"sales_price":"'  . $rs["sales_price"] . '",';
+        $outp .= '"num_rows":"'  . $num . '"}';
 	}
 
 	$outp ='['.$outp.']';
